764def60ee6d4c3b2d196d74815fbbcf11a68760
Saleh Alrashidi <sayed@emergegcc.com>
2026-04-10 19:41:05 +0300

    first version

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <iframe
+      src="/scheduler.html"
+      style={{
+        width: "100%",
+        height: "100vh",
+        border: "none"
+      }}
+      title="Scheduler"
+    />
+  )
+}
+
+export default App
